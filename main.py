79be1b835cfa419a97aa94d8fc1b175e69e5a1e7
Tamzamorano <tamara.zamorano@genstudents.org>
2024-11-26 15:35:10 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Lee la palabra y le asigna valor y la guarda en memoria
+
+nombre = input ("Ingresa tu nombre: ") #imprime un mensaje al usuario y espera que ingrese un dato
+apellido = input ("Ingresa tu apellido: ")
+ciudad = input ("Ingresa tu ciudad: ")
+
+print(f"Hola, {nombre} {apellido}. Seleccionaste la ciudad: {ciudad}")
+
+nombre_completo = f"{nombre} {apellido}"
+print(f"Nombre completo: {nombre_completo}")
